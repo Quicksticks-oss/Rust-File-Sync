afb36265e5031d91241e05eb0e02bc3ae4aa0f77
Quicksticks-oss <chongobongotoday@gmail.com>
2023-09-23 02:43:26 -0400

    first

---
+++ src/main.rs
@@ -0,0 +1,68 @@
+mod sync {
+    pub mod syncer;
+}
+use std::time::Instant;
+use std::error::Error;
+use std::fs;
+use std::env;
+
+use crate::sync::syncer;
+
+fn create_dir_non_exists(dir_path:&str){
+    // Check if the directory exists
+    if !fs::metadata(&dir_path).is_ok() {
+        // If it doesn't exist, create it
+        if let Err(err) = fs::create_dir(&dir_path) {
+            eprintln!("Error creating directory: {}", err);
+        } else {
+            println!("Directory created: {}", dir_path);
+        }
+    } else {
+        println!("Directory already exists: {}", dir_path);
+    }
+}
+
+fn add_trailing_slash(mut input: String) -> String {
+    if !input.ends_with('/') {
+        input.push('/');
+    }
+    input
+}
+
+fn print_help() {
+    println!("Usage: sync <source_path> <destination_path>");
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 3 {
+        print_help();
+        return Ok(());
+    }
+
+    let sync_source = String::from(&args[1]);
+    let sync_destination = String::from(&args[2]);
+
+    let sync_source_path = &add_trailing_slash(sync_source);
+    let sync_destination_path = &add_trailing_slash(sync_destination);
+    
+
+    create_dir_non_exists(sync_destination_path);
+
+    let sync_source_path_string: &String = &String::from(sync_source_path);
+    let sync_destination_path_string: &String = &String::from(sync_destination_path);
+
+    println!("Starting Sync...");
+    let start_time = Instant::now();
+
+    let source_paths = syncer::walk_folder(sync_source_path, &syncer::default_callback);
+    let dest_paths = syncer::walk_folder(sync_destination_path, &syncer::default_callback);
+
+    syncer::sync(sync_source_path_string, sync_destination_path_string, source_paths, dest_paths);
+
+    let end_time = Instant::now();
+    let duration = end_time - start_time;
+    println!("Time taken: {:.2} seconds", duration.as_secs_f64());
+
+    Ok(())
+}
---
+++ src/sync/syncer.rs
@@ -0,0 +1,129 @@
+use walkdir::WalkDir;
+use std::fs;
+
+pub fn default_callback(path:String, modified:String, file_name_str:String){
+    println!("Scanning: {} | File{}, Path: {}", modified, file_name_str, path);
+}
+
+pub fn create_dir(path:String){
+    // Attempt to create the directory
+    match fs::create_dir(&path) {
+        Ok(_) => {},
+        Err(_) => {},
+    }
+}
+
+pub fn delete_dir(path:String){
+    // Attempt to remove the directory
+    match fs::remove_dir_all(&path) {
+        Ok(_) => {},
+        Err(_) => {},
+    }
+}
+
+fn compare(source_path:&String, dest_path:&String, source: Vec<(String, String)>, dest: Vec<(String, String)>) -> (Vec<String>, Vec<String>){
+    // Find files to modify, create, or destroy
+    let mut creations = vec![];
+    let mut destructions = vec![];
+
+    // Compare source to dest
+    for (source_name, source_content) in &source {
+        let source_name_new = &source_name.replace(source_path, dest_path);
+        if let Some(_) = dest.iter().position(|(name, _)| name == source_name_new) {
+            if dest.contains(&(source_name_new.clone(), source_content.clone())) == false {
+                // Modify file.
+                creations.push(source_name.clone());
+            }
+        } else {
+            // Create new file.
+            creations.push(source_name.clone());
+        }
+    }
+
+    // Find files to destroy
+    for (dest_name_, _) in &dest {
+        let dest_name_str = dest_name_.replace(dest_path, &source_path);
+        let dest_name = &dest_name_str;
+        if !source.iter().any(|(source_name, _)| source_name == dest_name && source_path != dest_name && dest_path != dest_name) {
+            destructions.push(dest_name.clone());
+        }
+    }
+
+    if creations.len() > 0{
+        creations.remove(0);
+    }
+    if destructions.len() > 0{
+        destructions.remove(0);
+    }
+
+    return (creations, destructions);
+}
+
+pub fn sync(
+    source_path: &String,
+    dest_path: &String,
+    source: (Vec<(String, String)>, Vec<(String, String)>),
+    destination: (Vec<(String, String)>, Vec<(String, String)>),
+) {
+    // Extract source and destination file lists and directories
+    let (source_files, source_dirs) = source;
+    let (dest_files, destination_dirs) = destination;
+
+    let (dir_creations, dir_destructions) = compare(source_path, dest_path, source_dirs, destination_dirs);
+    let (file_creations, file_destructions) = compare(source_path, dest_path, source_files, dest_files);
+
+    // Create new dirs.
+    dir_creations.iter().for_each(|item| {
+        let new_dir = item.replace(source_path, dest_path);
+        create_dir(new_dir.clone());
+    });
+
+    // Delete dirs.
+    dir_destructions.iter().for_each(|item| {
+        let new_dir = item.replace(source_path, dest_path);
+        delete_dir(new_dir.clone());
+    });
+
+    // Copy Files
+    file_creations.iter().for_each(|item| {
+        let new_dir = item.replace(source_path, dest_path);
+        println!("Copy: {} to {}", item, new_dir);
+        match fs::copy(item, new_dir.clone()) {
+            Ok(_) => {},
+            Err(err) => eprintln!("Error: {}", err),
+        }
+    });
+
+    file_destructions.iter().for_each(|item| {
+        let new_file = item.replace(source_path, dest_path);
+        println!("Del: {}", new_file);
+        // Attempt to delete the file
+        match fs::remove_file(new_file) {
+            Ok(_) => println!("File deleted successfully."),
+            Err(e) => eprintln!("Error deleting file: {}", e),
+        }
+    });
+}
+
+pub fn walk_folder(source_path:&str, callback: &dyn Fn(String, String, String)) -> (Vec<(String, String)>, Vec<(String, String)>){
+    let mut files:Vec<(String, String)> = vec![];
+    let mut directories:Vec<(String, String)> = vec![];
+    for entry in WalkDir::new(source_path).follow_links(true) {
+        let entry = entry.expect("Could not walk dir.");
+        if entry.file_type().is_file() {
+            let file_name_str: String = entry.file_name().to_string_lossy().into_owned();
+            let full_file_path: String = entry.path().to_string_lossy().into_owned();
+            let file_size = entry.metadata().expect("Could not get metadata.").len(); // Get file size in bytes
+            files.push((full_file_path.clone(), file_size.to_string().clone()));
+            callback(full_file_path, file_size.to_string(), file_name_str);
+        }
+        else if entry.file_type().is_dir() {
+            let file_name_str: String = entry.file_name().to_string_lossy().into_owned();
+            let full_file_path: String = entry.path().to_string_lossy().into_owned();
+            let file_size = entry.metadata().expect("Could not get metadata.").len(); // Get file size in bytes
+            directories.push((full_file_path.clone(), file_name_str.clone()));
+            callback(full_file_path, file_size.to_string(), file_name_str);
+        }
+    }
+    return (files, directories);
+}
